3f913c92cabb5c183c1be37f7cd6bda845ef7745
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-09-17 15:33:57 -0700

    user can see all the records

---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,14 @@
+class RecordsController < ApplicationController
+  def index
+    @records = Record.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# Wipe the database
+Record.destroy_all
+# Let's create a bunch of records
+Record.create([
+  {
+    title: "On Avery Island",
+    artist: "Neutral Milk Hotel",
+    year: 1996,
+    cover_art: "https://en.wikipedia.org/wiki/File:On_avery_island_album_cover.jpg#/media/File:On_avery_island_album_cover.jpg",
+    song_count: 12
+  },
+  {
+    title: "Everything All the Time",
+    artist: "Band of Horses",
+    year: 2006,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/5/51/BandofHorsesEverythingalltheTime.jpg",
+    song_count: 10
+  },
+  {
+    title: "The Flying Club Cup",
+    artist: "Beirut",
+    year: 2007,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/4/4c/The_Flying_Club_Cup.jpg",
+    song_count: 13
+  }
+])
