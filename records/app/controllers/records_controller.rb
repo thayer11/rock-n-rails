7f35502292e18754fe8135b233aed481dc55de3b
J.P. Barela <barela@jpbarela.com>
2016-07-26 10:15:55 -0600

    Add new route

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -4,6 +4,10 @@ class RecordsController < ApplicationController
 		@records = Record.all
 	end
 
+	def new
+		@record = Record.new
+	end
+
 	def show 
 		@record = Record.find(params[:id])
 	end
--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :records, only: [:index, :show]
+  resources :records, only: [:index, :new, :show]
 
 end
