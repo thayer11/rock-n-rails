95eec19a25727803b571eef0590bd09556c71bd6
J.P. Barela <barela@jpbarela.com>
2016-07-26 09:59:00 -0600

    Add show route.

--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :records, only: [:index]
+  resources :records, only: [:index, :show]
 
 end
