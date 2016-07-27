5f7e77507f972e1fbfda38b3327ee95ab0e53c6c
thayer11 <holly.coddington@gmail.com>
2016-07-26 19:41:06 -0600

    Create

--- records/app/controllers/records_controller.rb
+++ records/app/controllers/records_controller.rb
@@ -5,4 +5,25 @@ def index
   # render :index
 end
 
+def show
+    @record = Record.find(params[:id])
+    render :show #optional
 end
+
+def new
+    @record = Record.new
+    render :new #optional
+end
+
+def create
+    Record.create(record_params)
+    redirect_to('/records')
+end
+
+private
+
+  def record_params
+    params.require(:record).permit(:title, :artist, :year, :conver_art, :song_count)
+  end
+end # end of class
+
--- records/config/routes.rb
+++ records/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :records, only: [:index]
+  resources :records, only: [:index, :new, :show]
+  resources :records, expect: [:delete, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
